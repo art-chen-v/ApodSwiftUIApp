771fa1c078164d7ea57840fc5c8694e0905157b9
art-chen-v <art.chen.v@gmail.com>
2025-02-12 22:56:50 +0900

    add start view

---
+++ ApodSwiftUIApp/Views/ApodSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApodSwiftUIAppApp.swift
+//  ApodSwiftUIApp
+//
+//  Created by Artem Chen on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ApodSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ApodsLazyList()
+        }
+    }
+}
