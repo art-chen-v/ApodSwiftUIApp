efeaaabb0624c451e7a59f190298b99204014dab
art-chen-v <art.chen.v@gmail.com>
2025-02-12 22:47:45 +0900

    add tests for single and multiple apod fetches

--- ApodSwiftUIAppTests/ApodSwiftUIAppTests.swift
+++ ApodSwiftUIAppTests/ApodSwiftUIAppTests.swift
@@ -9,28 +9,36 @@ import XCTest
 @testable import ApodSwiftUIApp
 
 final class ApodSwiftUIAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testApiClientDoesFetchSingleApodData() async throws {
+        let apiClient = ApodClient(downloader: TestDownloader())
+        let apod = try await apiClient.fetchLatestApod()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        if let date = dateFormatter.date(from: "2025-02-11") {
+            XCTAssertEqual(apod.date, date)
+        } else {
+            XCTFail()
         }
     }
-
+    
+    func testApiClientDoesFetchMultipleApodData() async throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        let apiClient = ApodClient(downloader: TestDownloader())
+        
+        if let earlierDate = dateFormatter.date(from: "2025-02-01"),
+            let laterDate = dateFormatter.date(from: "2025-02-11") {
+            let apods = try await apiClient.fetchApods(from: earlierDate, to: laterDate)
+            XCTAssertEqual(apods.count, 11)
+        } else {
+            XCTFail()
+        }
+    }
+    
 }
