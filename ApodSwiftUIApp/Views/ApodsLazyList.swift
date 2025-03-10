5500fbe027a59f23d64083a6f0424ed4dd1a95cd
art-chen-v <art.chen.v@gmail.com>
2025-03-10 18:47:54 +0900

    add preferenceKey for apod cell offset

--- ApodSwiftUIApp/Views/ApodsLazyList.swift
+++ ApodSwiftUIApp/Views/ApodsLazyList.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+struct ApodCellViewOffsetPreferenceKey: PreferenceKey {
+    
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
 struct ApodsLazyList: View {
     @StateObject var apodsProvider = ApodsProvider()
     
